80e2cc83be0817ea5064cddc97f2da857f781b03
regis <amaral.regis@gmail.com>
2024-03-07 12:23:33 -0300

    feat: Adiciona factories e seeder para preenchimento de dados
    Adiciona factories para criar registros falsos para as tabelas Patient e Address, e uma seeder para popular o banco de dados com esses registros falsos.

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Address;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Address::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'cep' => $this->faker->randomNumber(8),
+            'address' => $this->faker->streetAddress,
+            'number' => $this->faker->buildingNumber,
+            'complement' => $this->faker->optional()->secondaryAddress,
+            'neighborhood' => $this->faker->citySuffix,
+            'city' => $this->faker->city,
+            'state' => $this->faker->stateAbbr,
+        ];
+    }
+}
---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Address;
+use App\Models\Patient;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Patient::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'full_name' => $this->faker->name(),
+            'mother_name' => $this->faker->name(gender: 0),
+            'date_of_birth' => $this->faker->dateTimeBetween('-100 years', 'now', null),
+            'cpf' => $this->faker->unique()->buildingNumber(),
+            'cns' => $this->faker->unique()->buildingNumber(),
+            'address_id' => Address::factory()->create()->id,
+        ];
+    }
+}
