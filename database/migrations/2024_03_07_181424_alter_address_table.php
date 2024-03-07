1574b202a3c3e7e32f66aab2dec610924c4fb93d
regis <amaral.regis@gmail.com>
2024-03-07 15:51:01 -0300

    refactor: Altera a estrutura das tabelas Patient e Address
    Atualiza a estrutura das tabelas Patient e Address para corresponder melhor ao enunciado do desafio

---
+++ database/migrations/2024_03_07_181424_alter_address_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->renameColumn('address', 'street');
+            $table->unsignedBigInteger('patient_id')->nullable();
+            $table->foreign('patient_id')->references('id')->on('patients')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->renameColumn('street', 'address');
+            $table->dropForeign(['patient_id']);
+            $table->dropColumn('patient_id');
+        });
+    }
+};
