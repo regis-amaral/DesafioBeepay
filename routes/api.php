f04cc68c5e51110935151f7871477c2fe9fdda00
regis <amaral.regis@gmail.com>
2024-03-09 10:40:08 -0300

    refactor: Substitui rota padrão de recursos por rotas personalizadas

--- routes/api.php
+++ routes/api.php
@@ -19,10 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('patients', PatientController::class);
+Route::get('/patients', [PatientController::class, 'index'])->name('list_all_patients');
+
+Route::get('/patients/{id}', [PatientController::class, 'show'])->name('show_patient');
+
+Route::post('/patients/create', [PatientController::class, 'create'])->name('create_patient');
+
+Route::put('/patients/{id}/update', [PatientController::class, 'update'])->name('update_patient');
+
+Route::delete('/patients/{id}/delete', [PatientController::class, 'delete'])->name('delete_patient');
 
 Route::get('/search-cep/{cep}', [CepController::class, 'searchCep']);
 
 Route::post('/patients/upload-csv', [PatientController::class, 'uploadCsv']);
 
-Route::get('search', [PatientController::class, 'search']);
+Route::get('/patients/search', [PatientController::class, 'search'])->name('search_search');
