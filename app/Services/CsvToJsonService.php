4db34dc4690ae51227e9d1bfb3ad0c9c239a7c5a
regis <amaral.regis@gmail.com>
2024-03-10 04:20:59 -0300

    fix: Adicionadas validações na estrutura do arquivo csv

--- app/Services/CsvToJsonService.php
+++ app/Services/CsvToJsonService.php
@@ -7,7 +7,7 @@ class CsvToJsonService
     public static function convertPatientsList($filename = '', $delimiter = ','):array|false
     {
         if (!file_exists($filename) || !is_readable($filename)) {
-            return false;
+            throw new \RuntimeException("O arquivo '{$filename}' não pode ser aberto para leitura.");
         }
 
         $header = null;
@@ -18,14 +18,22 @@ class CsvToJsonService
                 if (!$header) {
                     $header = $row;
                 } else {
+                    if (empty($row)) {
+                        fclose($handle);
+                        throw new \InvalidArgumentException("O arquivo está vazio.");
+                    }
                     $rowData = [];
                     foreach ($row as $index => $value) {
-                        $key = $header[$index];
-                        if (strpos($key, 'address.') === 0) {
-                            $addressKey = str_replace('address.', '', $key);
-                            $rowData['address'][$addressKey] = $value;
-                        } else {
-                            $rowData[$key] = $value;
+                        if (isset($header[$index])) { // Verificar se o índice existe no cabeçalho
+                            $key = $header[$index];
+                            if (strpos($key, 'address.') === 0) {
+                                $addressKey = str_replace('address.', '', $key);
+                                $rowData['address'][$addressKey] = $value;
+                            } else {
+                                $rowData[$key] = $value;
+                            }
+                        }else{
+                            throw new \RuntimeException("O arquivo possui erro de formatação de dados.");
                         }
                     }
                     $data[] = $rowData;
